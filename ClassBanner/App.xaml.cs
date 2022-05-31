40caa3d14c7bb3a0b6e669160c61a2f7f1670fb8
Josh B <Josh B@DESKTOP-DU02SUQ>
2022-05-30 23:53:27 -0600

    Add project files.

---
+++ ClassBanner/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfScreenHelper;
+using System.Windows.Shapes;
+using ClassBanner;
+
+
+
+namespace ClassBanner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void CreateBannerWindowObj(Screen s,bool ShowOnBottom = false) {
+            var mainWindow = new MainWindow();
+            Rect r = s.WorkingArea;
+            if (ShowOnBottom)
+            {
+                mainWindow.Top = (r.Bottom / s.ScaleFactor) - mainWindow.Height;
+            }
+            else
+            {
+                mainWindow.Top = r.Top / s.ScaleFactor;
+            }
+            mainWindow.Left = r.Left / s.ScaleFactor;
+            mainWindow.Width = r.Width / s.ScaleFactor;
+
+            mainWindow.Show();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            List<MainWindow> MainWindowList = new List<MainWindow>();
+           bool check =  Utils.TestRegPath(@"HKEY_LOCAL_MACHINE\SOFTWARE\ClassBanner\");
+            bool ShowOnBottom = true;
+            foreach (var ss in Screen.AllScreens)
+                {
+                this.CreateBannerWindowObj(ss);
+                if (ShowOnBottom) {
+                    this.CreateBannerWindowObj(ss, true);
+                    }
+                }
+        }
+    }
+}
---
+++ ClassBanner/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using System.Windows.Shapes;
+using WpfScreenHelper;
+
+namespace ClassBanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    //
+    public partial class MainWindow : Window
+    {
+        private bool FadedOut = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+
+
+          /*  if (this.FadedOut) {
+                //this.Show();
+                var anim = new DoubleAnimation(100, (Duration)TimeSpan.FromSeconds(3), FillBehavior.Stop);
+                anim.Completed += (s, _) => this.Window_Show();
+                this.BeginAnimation(UIElement.OpacityProperty, anim);
+            }*/
+        }
+        private void Window_Hide() {
+            this.FadedOut = true;
+            this.Hide();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += (s, _) => this.Show(); ;
+            timer.Start();
+        }
+        private void Window_Show()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += (s,_) => this.Show(); ;
+            timer.Start();
+            this.FadedOut = false;
+            
+        }
+        private void Window_MouseEnter(object sender, EventArgs e)
+        {
+
+            var anim = new DoubleAnimation(0, (Duration)TimeSpan.FromSeconds(1), FillBehavior.Stop);
+            anim.Completed += (s, _) => this.Window_Hide();
+            this.BeginAnimation(UIElement.OpacityProperty, anim);
+        }
+
+
+
+    }
+}
